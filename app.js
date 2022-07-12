09ca98ebc76ac2cbfeaaf942b839065aa75259ea
bashar alzrigat <bashar.zureikat@gmail.com>
2022-07-13 01:23:29 +0300

    solve extra task

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 'use strict';
 const allEmployees = []
+const allDepartments = ["Administration", "Marketing", "Development", "Finance"]
+let switcher
+// Create the containers divs:
 const eContainer = document.createElement("div");
+const Administration = document.createElement("div");
+const Marketing = document.createElement("div");
+const Development = document.createElement("div");
+const Finance = document.createElement("div");
+//append the containers to the DOM:
+document.body.appendChild(eContainer);
+eContainer.appendChild(Administration);
+eContainer.appendChild(Marketing);
+eContainer.appendChild(Development);
+eContainer.appendChild(Finance);
+// add id to each container:
+eContainer.id = "employee-container";
+Administration.id = "Administration";
+Marketing.id = "Marketing";
+Development.id = "Development";
+Finance.id = "Finance";
 // Employee Constructor Function:
 function Employee(fullName, department, level, imageUrl) {
 
@@ -9,9 +28,9 @@ function Employee(fullName, department, level, imageUrl) {
     this.level = level
     this.imageUrl = imageUrl
     allEmployees.push(this);
-    // console.log(this);
 }
 // create a new employee object:
+new Employee("Bashar Alzrigat", "IT", "Senior", "");
 new Employee("Ghazi Samer", "Administration", "Senior", "assets/Ghazi.jpg");
 new Employee("Lana Ali", "Finance", "Senior", "assets/Lana.jpg");
 new Employee("Tamara Ayoub", "Marketing", "Senior", "assets/Tamara.jpg");
@@ -20,7 +39,7 @@ new Employee("Omar Zaid", "Development", "Senior", "assets/Omar.jpg");
 new Employee("Rana Saleh", "Development", "Junior", "assets/Rana.jpg");
 new Employee("Hadi Ahmad", "Finance", "Mid-Senior", "assets/Hadi.jpg");
 
-// calculate the net salary (tax 7.5%):
+// calculate the salary of each employee:
 Employee.prototype.Salary = function () {
     if (this.level === "Senior") {
         return Math.floor(Math.random() * (2000 - 1500 + 1)) + 1500;
@@ -30,17 +49,15 @@ Employee.prototype.Salary = function () {
         return Math.floor(Math.random() * (1000 - 500 + 1)) + 500;
     }
 }
-// generate a unique id for each employee:
+// generate a unique id# for each employee:
 Employee.prototype.Id = function () {
     return Math.ceil(Math.random() * 10000)
 }
 // render the employee object to the DOM:
-document.body.appendChild(eContainer);
-eContainer.id="employee-container";
-Employee.prototype.render = function () {
+Employee.prototype.render = function (switcher) {
     // create employeeCard div inside the eContainer div:
     const eCard = document.createElement("div");
-    eContainer.appendChild(eCard);
+    switcher.appendChild(eCard);
     eCard.className = "employee-card"
     // [IMG] create employeeImgDiv div inside the employeeCard div:
     const eImgDiv = document.createElement("div");
@@ -71,10 +88,14 @@ Employee.prototype.render = function () {
 
 function renderAllEmployees() {
     for (let i = 0; i < allEmployees.length; i++) {
-        allEmployees[i].render();
+        for (let j = 0; j < allDepartments.length; j++) {
+            if (allEmployees[i].department === allDepartments[j]) {
+                 switcher = document.getElementById(allDepartments[j]);
+                allEmployees[i].render(switcher);
+            }
+        }
     }
 }
-
 
 renderAllEmployees()
     // eInfoDiv.textContent = `Name: ${this.fullName} - ID: ${this.Id()}\n Department: ${this.department} - Level: ${this.level}\n Salary: ${this.Salary()}`;
